b07a62b18fb73b36b5ee30e6173a934570ba51d5
Pete <maestro_pete@inbox.ru>
2020-01-06 19:01:30 +0300

    поправил header

--- config.js
+++ config.js
@@ -5,7 +5,8 @@ let config = {
     'src/assets/bootstrap-3.4.1-dist/css/bootstrap.css' : 'css/',
     'src/assets/bootstrap-3.4.1-dist/js/bootstrap.js' : 'js/',
     'src/assets/jquery-3.4.1.min.js' : 'js/',
-    'src/assets/fontawesome-free-5.12.0-web/css/all.min.css' : 'css'
+    'src/assets/fontawesome-free-5.12.0-web/css/all.min.css' : 'css/',
+    'src/assets/fontawesome-free-5.12.0-web/js/all.js' : 'js/',
   }
 };
 
