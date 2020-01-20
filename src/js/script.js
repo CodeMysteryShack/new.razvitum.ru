4c3589dae9ac51b5c5b79e1637109cdc87d33c39
Pete <maestro_pete@inbox.ru>
2020-01-20 06:58:03 +0300

    стилизовал полностью section и article

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,25 @@
-// function ready(fn) {
-//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-//     fn();
-//   } else {
-//     document.addEventListener('DOMContentLoaded', fn);
-//   }
-// }
-//
-// ready(function(){
-//   console.log('DOM ready');
-// });
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(function(){
+  console.log('DOM ready');
+
+  const burgerBtn = document.querySelector('.navbar-toggle');
+  const menuWrap = document.querySelector('.menu-wrap');
+  const mainMenu = document.querySelector('#main-menu');
+  burgerBtn.addEventListener('click', showPhoneMenu);
+  function showPhoneMenu() {
+    menuWrap.classList.toggle('z-index');
+    if (mainMenu.style.display === "none") {
+      mainMenu.style.display = "block";
+    }
+    else mainMenu.style.display = "none"
+  }
+});
+
+
